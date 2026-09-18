d40fde4b93e5f685c8bcd7133523a3de7311ab52
Mutus <mateus08142@gmail.com>
2026-09-17 21:14:57 -0300

    Função de excluir

--- cadastros.py
+++ cadastros.py
@@ -1,13 +1,93 @@
 from database import conectar
 
-def cadastro()
-     
-while True:
-        
+
+def criar_usuario():
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    nome_usuario = input('Qual é o nome do usuário? ')
+
+    sql = """
+    SELECT NOME
+    FROM USUARIOS
+    WHERE NOME = %s
+    """
+
+    cursor.execute(sql, (nome_usuario,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        print('Usuário já cadastrado!')
+        cursor.close()
+        conexao.close()
+        return
+
+    senha_usuario = input('Qual a senha do usuário? ')
+
+    sql = """
+    INSERT INTO USUARIOS
+    (nome, senha)
+    VALUES (%s, %s)
+    """
+
+    cursor.execute(sql, (nome_usuario, senha_usuario))
+    conexao.commit()
+
+    cursor.close()
+    conexao.close()
+
+    print('Usuário cadastrado!')
+
+
+def excluir_usuario():
+
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    nome_usuario = input('Qual é o nome do usuário?')
+
+    sql = """
+    SELECT NOME
+    FROM USUARIOS
+    WHERE NOME = %s
+    """
+    cursor.execute(sql, (nome_usuario,))
+    resultado = cursor.fetchone()
+
+    if resultado:
+        escolha = input('Tem certeza disso? S/N ').lower()
+
+        if escolha == 's':
+
+        sql = """
+        DELETE 
+        FROM USUARIOS
+        WHERE NOME = %s
+        """
+
+        cursor.execute(sql, (nome_usuario,))
+        resultado = cursor.fetchone()
+        conexao.commit()
+
+        print('Usuário excluido...')
+
+    else:
+        print('Usuário não encontrado!')
+
+    cursor.close()
+    conexao.close()
+
+
+def cadastro():
+
+    while True:
+
         print('''
         1 - Cadastrar Usuário
         2 - Excluir Usuário
-        3 - Voltar''')
+        3 - Voltar
+        ''')
 
         escolha = int(input('Escolha uma opção: '))
 
@@ -21,46 +101,8 @@ while True:
 
         elif escolha == 3:
             print('Voltando...')
-            break
+            return
 
         else:
-            print('Opção invalida')
+            print('Opção inválida!')
             continue
-
-        def criar_usuario():
-
-
-            conexao = conectar()
-            cursor = conexao.cursor()
-
-            nome_usuario = input('Qual é o nome do usuário? ')
-
-            sql = """
-            SELECT NOME
-            FROM USUARIOS
-            WHERE NOME = %s
-            """
-
-            cursor.execute(sql, (nome_usuario,))
-            resultado = cursor.fetchone()
-
-            if resultado:
-
-                print('Usuário já Cadastrado!')
-                return
-
-            else:
-                senha_usuario = input('Qual a senha do usuário? ')
-                
-                sql = """ 
-                INSERT INTO USUARIOS
-                (nome, senha)
-                VALUES (%s , %s)
-                """
-                
-                cursor.execute(sql, (nome_usuario, senha_usuario))
-                conexao.commit()
-                cursor.close()
-                conexao.close()
-                
-                print('Usuário Cadastrado!')
